3934a5780ae96a487098216f8b38d556c637f885
Luke <115109073+Luke-Bytes@users.noreply.github.com>
2024-11-09 19:03:28 +0000

    Update GameInstance to a Singleton

--- src/database/GameInstance.ts
+++ src/database/GameInstance.ts
@@ -5,11 +5,9 @@ import { MapVoteManager } from "../logic/MapVoteManager";
 import { MinerushVoteManager } from "logic/MinerushVoteManager";
 import { MojangAPI } from "api/MojangAPI";
 
-// wrapper class for Game
-// TODO refactor this into a singleton
 export class GameInstance {
+  private static instance: GameInstance;
   gameId?: string;
-
   finished?: boolean;
   announced = false;
   startTime?: Date;
@@ -19,13 +17,19 @@ export class GameInstance {
     bannedClasses?: $Enums.AnniClass[];
     map?: $Enums.AnniMap;
   };
-
   teams: Record<Team, PlayerInstance[]> = { RED: [], BLUE: [] };
   mapVoteManager?: MapVoteManager;
   minerushVoteManager?: MinerushVoteManager;
 
-  constructor() {
+  private constructor() {
     this.settings = {};
+  }
+
+  public static getInstance(): GameInstance {
+    if (!GameInstance.instance) {
+      GameInstance.instance = new GameInstance();
+    }
+    return GameInstance.instance;
   }
 
   public reset() {
@@ -42,7 +46,9 @@ export class GameInstance {
     this.teams = { RED: [], BLUE: [] };
     this.mapVoteManager = undefined;
     this.minerushVoteManager = undefined;
+    PlayerInstance.resetAll();
   }
+
 
   public startMinerushVote() {
     this.minerushVoteManager = new MinerushVoteManager();
@@ -62,13 +68,11 @@ export class GameInstance {
     if (this.mapVoteManager) {
       this.mapVoteManager.cancelVote();
     }
-
     this.settings.map = map;
   }
 
   public async announce() {
     this.announced = true;
-
     if (this.mapVoteManager) {
       await this.mapVoteManager.startMapVote();
     }
@@ -83,34 +87,16 @@ export class GameInstance {
     )[0] as Team;
   }
 
-  public async addPlayerByDiscordId(
-    discordSnowflake: Snowflake,
-    ignUsed: string
-  ) {
+  public async addPlayerByDiscordId(discordSnowflake: Snowflake, ignUsed: string) {
     const player = await PlayerInstance.byDiscordSnowflake(discordSnowflake);
     const uuid = await MojangAPI.usernameToUUID(ignUsed);
-    if (!uuid) {
-      return {
-        error: "This in-game name doesn't exist.",
-      } as const;
-    }
-
-    if (!player.minecraftAccounts.includes(uuid)) {
-      return {
-        error:
-          "You have not registered this in-game name. Please use `/ign add`",
-      } as const;
-    }
-
+    if (!uuid) return { error: "This in-game name doesn't exist." } as const;
+    if (!player.minecraftAccounts.includes(uuid))
+      return { error: "You have not registered this in-game name. Please use `/ign add`" } as const;
     player.ignUsed = ignUsed;
-
     const team = this.getTeamWithLeastPlayers();
     this.teams[team].push(player);
-
-    return {
-      error: false,
-      team: team,
-    } as const;
+    return { error: false, team: team } as const;
   }
 
   public getPlayers() {
@@ -128,18 +114,13 @@ export class GameInstance {
 
   public shuffleTeams(shuffleMethod: "random") {
     switch (shuffleMethod) {
-      case "random":
-        {
-          const shuffled = this.getPlayers().sort(() => Math.random() - 0.5);
-          const half = Math.ceil(shuffled.length / 2);
-
-          const blue = shuffled.slice(0, half);
-          const red = shuffled.slice(half);
-
-          this.teams.BLUE = blue;
-          this.teams.RED = red;
-        }
+      case "random": {
+        const shuffled = this.getPlayers().sort(() => Math.random() - 0.5);
+        const half = Math.ceil(shuffled.length / 2);
+        this.teams.BLUE = shuffled.slice(0, half);
+        this.teams.RED = shuffled.slice(half);
         break;
+      }
     }
   }
 
@@ -149,20 +130,13 @@ export class GameInstance {
 
   public setTeamCaptain(team: Team, player: PlayerInstance) {
     const oldTeamCaptain = this.getCaptainOfTeam(team);
-
-    if (oldTeamCaptain) {
-      oldTeamCaptain.captain = false;
-    }
-
+    if (oldTeamCaptain) oldTeamCaptain.captain = false;
     player.captain = true;
-
     if (!this.teams[team].includes(player)) {
       const otherTeam = team === "RED" ? "BLUE" : "RED";
-
       if (this.teams[otherTeam].includes(player)) {
-        this.teams[otherTeam].splice(this.teams[otherTeam].indexOf(player));
+        this.teams[otherTeam].splice(this.teams[otherTeam].indexOf(player), 1);
       }
-
       this.teams[team].push(player);
     }
     return {
